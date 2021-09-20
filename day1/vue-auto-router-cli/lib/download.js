c7da32ab09336c28ac05906f81ecc3b787fe72e6
巴拉巴拉大法师 <1256325972@qq.com>
2021-09-20 23:15:26 +0800

    Change the color of the progress bar

--- day1/vue-auto-router-cli/lib/download.js
+++ day1/vue-auto-router-cli/lib/download.js
@@ -5,7 +5,11 @@ module.exports.clone = async function(repo, desc){
     const download = promisify(require('download-git-repo'));
     const ora = require('ora')
     const process = ora(`下载...${repo}`)
-    process.start('start')
+
+    // 改变颜色
+    process.color = 'yellow'
+    process.start("Loading...")
+    
     await download(repo,desc);
     process.succeed('sucess')
 }
