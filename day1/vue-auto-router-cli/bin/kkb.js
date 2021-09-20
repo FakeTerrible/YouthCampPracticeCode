953dd12d324276651a270dae4ec3a8401156a2c6
巴拉巴拉大法师 <1256325972@qq.com>
2021-09-20 14:50:47 +0800

    finish

--- day1/vue-auto-router-cli/bin/kkb.js
+++ day1/vue-auto-router-cli/bin/kkb.js
@@ -5,5 +5,11 @@ program.version(require("../package.json").version);
 program
   .command("init <name>")
   .description("init project")
-  .action(require('../lib/init'));
+  .action(require("../lib/init"));
+
+program
+  .command("refresh")
+  .description("refresh routers...")
+  .action(require("../lib/refresh"));
+  
 program.parse(process.argv);
--- day1/vue-auto-router-cli/lib/init.js
+++ day1/vue-auto-router-cli/lib/init.js
@@ -6,6 +6,20 @@ const clear = require('clear')
 const chalk = require('chalk')
 const log = content => console.log(chalk.green(content))
 const {clone} = require('./download')
+const open = require('open')
+
+const spawn = async(...args)=>{
+
+    const {spawn} = require('child_process');
+    return new Promise(resolve => {
+        const proc = spawn(...args)
+        proc.stdout.pipe(process.stdout)
+        proc.stderr.pipe(process.stderr)
+        proc.on('close', () => {
+            resolve()
+        })
+    })
+}
 
 module.exports = async name => {
     clear()
@@ -14,6 +28,19 @@ module.exports = async name => {
 
     // 项目模板
     log('创建项目：'+name)
-    await clone('https://github.com/su37josephxia/vue-template.git',name);
+    await clone('github:su37josephxia/vue-template',name);
 
+    log('安装依赖...')
+    await spawn('npm', ['install'], {cwd:`./${name}`})
+    
+    log(chalk.green(`
+    👌安装完成：
+    To get Start:
+    ===========================
+        cd ${name}
+        npm run serve
+    ===========================
+                `))
+    open('http://localhost:8080')
+    await spawn('npm',['run','serve'],{cwd:`./${name}`})
 }
