cbc35021f1acea9118ecd6c2342f015052c0c0a6
巴拉巴拉大法师 <1256325972@qq.com>
2021-09-21 14:12:29 +0800

    add start

--- day2/2x/webpack_learn/webpack.config.js
+++ day2/2x/webpack_learn/webpack.config.js
@@ -1,11 +1,23 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
-  mode: "production",
+  mode: "development",
   entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "练习",
+    }),
+  ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   module: {
     rules: [
